ffeb5fc6fa1900d906c96b647cbe7d47bbf5f8b5
Vineeth <vineeth@autonomic.ai>
2026-03-22 18:16:08 -0400

    Print Page Size

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,37 +3,34 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-public class Main {
-  public static void main(String[] args){
-    if (args.length < 2) {
-      System.out.println("Missing <database path> and <command>");
-      return;
-    }
+void main(String[] args) {
+  if (args.length < 2) {
+    System.out.println("Missing <database path> and <command>");
+    return;
+  }
 
-    String databaseFilePath = args[0];
-    String command = args[1];
+  String databaseFilePath = args[0];
+  String command = args[1];
 
-    switch (command) {
-      case ".dbinfo" -> {
-        try {
-          FileInputStream databaseFile = new FileInputStream(new File(databaseFilePath));
-          
-          databaseFile.skip(16); // Skip the first 16 bytes of the header
-          byte[] pageSizeBytes = new byte[2]; // The following 2 bytes are the page size
-          databaseFile.read(pageSizeBytes);
-          short pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort();
-          int pageSize = Short.toUnsignedInt(pageSizeSigned);
+  switch (command) {
+    case ".dbinfo" -> {
+      try {
+        FileInputStream databaseFile = new FileInputStream(new File(databaseFilePath));
 
-          // You can use print statements as follows for debugging, they'll be visible when running tests.
-          System.err.println("Logs from your program will appear here!");
+        databaseFile.skip(16); // Skip the first 16 bytes of the header
+        byte[] pageSizeBytes = new byte[2]; // The following 2 bytes are the page size
+        databaseFile.read(pageSizeBytes);
+        short pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort();
+        int pageSize = Short.toUnsignedInt(pageSizeSigned);
 
-          // TODO: Uncomment the code below to pass the first stage
-          // System.out.println("database page size: " + pageSize);
-        } catch (IOException e) {
-          System.out.println("Error reading file: " + e.getMessage());
-        }
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.err.println("Logs from your program will appear here!");
+
+        System.out.println("database page size: " + pageSize);
+      } catch (IOException e) {
+        System.out.println("Error reading file: " + e.getMessage());
       }
-      default -> System.out.println("Missing or invalid command passed: " + command);
     }
+    default -> System.out.println("Missing or invalid command passed: " + command);
   }
 }
